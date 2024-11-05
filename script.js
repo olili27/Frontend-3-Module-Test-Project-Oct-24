82ef9c0883b2cbefde1cfedb36df07233ab8694b
olili27 <timmolili27@gmail.com>
2024-11-06 01:48:57 +0300

    declare store with `let`

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const url =
   "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
 
-  const store = null
+  let store = null
 
 async function displayData(data) {
   store = [...data];
